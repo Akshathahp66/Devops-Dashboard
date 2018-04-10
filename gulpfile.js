04e7f59126976822b1fa872f6b09f0e8c8267682
10029945 <akshhp@univar.com>
2018-04-10 15:50:53 +0530

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: "app/html/homepage.html"
+        },
+    })
+  })
+
+gulp.task('watch', ['browserSync'], function (){
+    gulp.watch('app/css/**/*.css', browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload);
+    
+  })
