e73804c11a6f41a98446496870a81f31edb80365
10029945 <akshhp@univar.com>
2018-07-22 22:53:42 +0530

    survey page changes

--- app/js/global.js
+++ app/js/global.js
@@ -1,5 +1,7 @@
 new WOW().init();
 $('#header').load('../../app/html/header.html', function(){
 });
+$('#survey').load('../../app/html/survey.html', function(){
+});
 $('#footer').load('../../app/html/footer.html', function(){
 });
